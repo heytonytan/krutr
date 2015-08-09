925685054e74cc5c22b5626a15b749d7ba256ea8
Kwan Tiong Tan <tonyktan@Kwans-MacBook-Pro.local>
2015-08-09 14:22:01 +0800

    add slide deck component

--- components/main.jsx
+++ components/main.jsx
@@ -1,11 +1,14 @@
 var Profile = React.createClass({
   render: function(){
     return ( 
-        <Slide />
+      <div className="profile">
+        <ProfilePic />
+        <SlideDeck />
+      </div>
     );
   }
 });
-var Pic = React.createClass({
+var ProfilePic = React.createClass({
   render: function(){
     return (
       <div className="profilePic">
@@ -15,16 +18,39 @@ var Pic = React.createClass({
     );
   }
 });
-var Slide = React.createClass({
+var SlideDeck = React.createClass({
+  getInitialState: function(){
+    return {
+      data: [
+        {
+          id: "4234234",
+          description: "Graduated from the University of Pennsylvania with a BSc in Computer Science",
+          picture: "http://www.natcom.org/uploadedImages/More_Scholarly_Resources/Doctoral_Program_Resource_Guide/UPenn%20Campus.jpg"
+        },
+        {
+          id: "4234235",
+          description: "Presently works at Box",
+          picture: "http://hackathon-in-a-box.org/img/box.png"
+        },
+        {
+          id: "4234236",
+          description: "Loves Scuba and badminton",
+          picture: "http://www.aqua-sport.com/_Uploads/dbsArticles/DivingCourse1.JPG"
+        }
+      ]
+    };
+  },
   render: function(){
-    return (
-      <div className="slide">
-        <h2>Loves to dive</h2>
-        <img src="http://www.aqua-sport.com/_Uploads/dbsArticles/DivingCourse1.JPG"/>
-      </div>
-    );
+    this.props.data.map(function(slide){
+      return (
+        <div className="slide">
+          <h2>{slide.description}</h2>
+          <img src={slide.picture} />
+        </div>
+      );
+    });
   }
 });
 React.render(
-  <Profile />, document.getElementById('profile')
+  <Profile />, document.getElementById('content')
 );
